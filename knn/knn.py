5637558428771ad1a002c711f09095952b39f503
JadGh7 <jado-2003@live.com>
2024-09-16 18:52:58 +0300

    Adding Files

---
+++ knn/knn.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
+
+dataset = pd.read_csv('diabetes.csv')
+print(len(dataset))
